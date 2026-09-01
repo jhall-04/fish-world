3c2b1093bce0a7ffed4d2156ef6af40454b551b9
jhall-04 <jjordanjh004@outlook.com>
2026-08-31 20:33:55 -0400

    Boid

---
+++ boid.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+class Boid {
+private:
+    float x;
+    float y;
+    float theta;
+    float velocity;
+public:
+    Boid(float x, float y, float theta, float velocity) {
+        x = x
+        y = y
+        theta = theta
+        velocity = velocity
+    }
+}
+
+class Flock {
+    vector<Boid> boids = []
+}
